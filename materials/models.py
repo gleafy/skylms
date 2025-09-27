f712872f45c3cd5c61b35cdabe11e30162d3347c
gleafy <greenleafy@yandex.ru>
2025-09-27 22:40:14 +0400

    Обновил модели materials

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    preview = models.ImageField(upload_to="course_previews/", null=True, blank=True)
+    description = models.TextField()
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    preview = models.ImageField(upload_to="lesson_previews/", null=True, blank=True)
+    video_link = models.URLField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
