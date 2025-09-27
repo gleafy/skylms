7e46948824e15fefee930bf63caf4e184237d9e5
gleafy <greenleafy@yandex.ru>
2025-09-27 21:29:00 +0400

    Обновил модели users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    city = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
