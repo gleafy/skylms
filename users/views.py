371fc972675089b7472f57b2e447042516f9047d
gleafy <greenleafy@yandex.ru>
2025-10-05 15:00:00 +0400

    Обновил представления users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Payment
+from .serializers import PaymentSerializer
 
-# Create your views here.
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["course", "lesson", "payment_method"]
+    ordering_fields = ["payment_date"]
