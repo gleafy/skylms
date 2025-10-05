3fee2181b69000a1ccb8ce6783a50143b920d064
gleafy <greenleafy@yandex.ru>
2025-10-05 15:00:00 +0400

    Обновил модели users

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
@@ -11,3 +10,16 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+class Payment(models.Model):
+    PAYMENT_METHODS = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    course = models.ForeignKey("materials.Course", on_delete=models.SET_NULL, null=True, blank=True)
+    lesson = models.ForeignKey("materials.Lesson", on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS)
