2af613ed957f6e6da73553e71c7e8bd38e6fd623
gleafy <greenleafy@yandex.ru>
2025-10-12 00:00:00 +0400

    Обновил представления users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Payment
-from .serializers import PaymentSerializer
+from .models import Payment, User
+from .serializers import PaymentSerializer, UserSerializer, UserCreateSerializer
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
