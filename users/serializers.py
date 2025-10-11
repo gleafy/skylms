71d9a125b1de64aeeb47af36115405bc15dc9dbb
gleafy <greenleafy@yandex.ru>
2025-10-12 00:00:00 +0400

    Обновил сериализатор users

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
-from .models import Payment
+from .models import User, Payment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'phone', 'city', 'avatar']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
