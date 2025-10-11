d031077ee0c13ec0e5be32b988934398d50cd321
gleafy <greenleafy@yandex.ru>
2025-10-12 00:00:00 +0400

    Обновил модели materials

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.conf import settings
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
     preview = models.ImageField(upload_to="course_previews/", null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     description = models.TextField()
 
 
@@ -11,5 +12,6 @@ class Lesson(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     preview = models.ImageField(upload_to="lesson_previews/", null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     video_link = models.URLField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
