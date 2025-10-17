9271cb8b0d4fcbb385a1d42868da59d827b9beba
gleafy <greenleafy@yandex.ru>
2025-10-18 01:00:00 +0400

    создал тесты users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import User, Payment
 
-# Create your tests here.
+class PaymentTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(email='test@test.com')
+        self.payment = Payment.objects.create(
+            user=self.user,
+            amount=1000,
+            payment_method="cash"
+        )
+
+    def test_payment_list(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(reverse('users:payment-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
