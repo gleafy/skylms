a8ae1c76ea7fe1c19e00deca5bb784334ab0c4b5
gleafy <greenleafy@yandex.ru>
2025-10-18 01:00:00 +0400

    обновил сериализатор материалов

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Course, Lesson
+from .validators import validate_youtube_url
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+    def validate_video_link(self, value):
+        validate_youtube_url(value)
+        return value
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
