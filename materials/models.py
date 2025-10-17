2d08a2fc8784ee0383b276eda58554f93008cff7
gleafy <greenleafy@yandex.ru>
2025-10-18 01:00:00 +0400

    Обновил модель materials

--- materials/models.py
+++ materials/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from .validators import validate_youtube_url
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
@@ -7,11 +8,10 @@ class Course(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     description = models.TextField()
 
-
 class Lesson(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     preview = models.ImageField(upload_to="lesson_previews/", null=True, blank=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    video_link = models.URLField()
+    video_link = models.URLField(validators=[validate_youtube_url])
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
