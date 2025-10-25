c9b9550cb3d4dc5f8a8b82255b84cbb3f38f3b37
gleafy <greenleafy@yandex.ru>
2025-10-25 18:49:00 +0400

    Обновил модели materials

--- materials/models.py
+++ materials/models.py
@@ -7,6 +7,7 @@ class Course(models.Model):
     preview = models.ImageField(upload_to="course_previews/", null=True, blank=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     description = models.TextField()
+    subscribers = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Subscription', related_name='subscribed_courses')
 
 class Lesson(models.Model):
     title = models.CharField(max_length=200)
@@ -15,3 +16,11 @@ class Lesson(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     video_link = models.URLField(validators=[validate_youtube_url])
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='subscriptions')
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'course']
